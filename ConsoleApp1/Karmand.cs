01450705af53613afa66db66211f60203b9d2f02
Alireza <119754279+Sheikhly@users.noreply.github.com>
2023-07-30 09:33:51 +0330

    First commit

---
+++ ConsoleApp1/Karmand.cs
@@ -0,0 +1,12 @@
+namespace ConsoleApp1;
+
+public class Karmand
+{
+    private string name; // field
+
+    public string Name   // property
+    {
+        get { return name; }   // get method
+        set { name = value; }  // set method
+    }
+}
